d8465caaf523d0c8090de68298c4333ead99837a
Somen Singha <somensingha06@gmail.com>
2022-05-07 17:04:37 +0600

    added

--- src/components/RequireAuth/RequireAuth.js
+++ src/components/RequireAuth/RequireAuth.js
@@ -1,22 +1,22 @@
-// import React from "react";
+import React from "react";
 
-// import { Navigate, useLocation } from "react-router-dom";
-// import auth from "./../../firebase.init";
-// import { useAuthState } from "react-firebase-hooks/auth";
-// import Loading from "../Loading/Loading";
+import { Navigate, useLocation } from "react-router-dom";
+import auth from "./../../firebase.init";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Loading from "../Loading/Loading";
 
-// const RequireAuth = ({ children }) => {
-//   const [user, loading] = useAuthState(auth);
+const RequireAuth = ({ children }) => {
+  const [user, loading] = useAuthState(auth);
 
-//   const location = useLocation();
-//   if (loading) {
-//     return <Loading></Loading>;
-//   }
+  const location = useLocation();
+  if (loading) {
+    return <Loading></Loading>;
+  }
 
-//   if (!user) {
-//     return <Navigate to="/login" state={{ from: location }} replace />;
-//   }
-//   return children;
-// };
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  return children;
+};
 
-// export default RequireAuth;
+export default RequireAuth;
